191f9c3ed0cbe81c5c58f6a8fbf458d6fb49102c
shohag390 <shohag390@users.noreply.github.com>
2025-05-11 13:56:14 +0600

    Navbar Ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,18 +37,18 @@ const Navbar = () => {
     }
 
     return (
-        <nav className='h-[10vh] container mx-auto md:px-[90px] w-full flex items-center justify-between'>
+        <nav className='lg:h-[10vh] lg:py-0 py-[8px] container mx-auto lg:px-[90px] md:px-[50px] px-[20px] bg-[#fff] w-full flex items-center justify-between border-b-[1px] border-[#e6e6e6]'>
             {/* Nav Logo */}
             <Link to={"/"}>
-                <img className='h-[40px]' src={logo} alt="Logo" />
+                <img className='lg:h-[40px] md:h-[35px] h-[30px]' src={logo} alt="Logo" />
             </Link>
 
             {/* Nav Midle Section */}
-            <ul className='flex items-center gap-[30px]'>
+            <ul className='lg:flex md:items-center md:gap-[30px] hidden'>
                 {
                     navLink?.map((link) => (
                         <li>
-                            <NavLink to={link?.path}>{link?.display}</NavLink>
+                            <NavLink className={className => className?.isActive ? "text-[#6442f9] pb-[2px] border-b-[2px] border-[#6442f9] font-medium" : "text-[#0a0618] font-medium"} to={link?.path}>{link?.display}</NavLink>
                         </li>
                     ))
                 }
@@ -58,12 +58,12 @@ const Navbar = () => {
             </ul>
 
             {/* Nav  Right Section */}
-            <div className='flex items-center gap-[30px]'>
-                <Link to={"cart"}>
+            <div className='flex items-center lg:gap-[30px] md:gap-[25px] gap-[20px]'>
+                <Link className='h-[40px] w-[40px] relative flex items-center justify-center text-[#0a0618]' to={"cart"}>
                     <div className='text-[30px]'>
                         <IoMdCart />
                     </div>
-                    <div className='h-[8px] w-[8px] bg-[#00c073] rounded-full'></div>
+                    <div className='h-[8px] absolute top-[5px] left-[30px] w-[8px] bg-[#6442f9] rounded-full'></div>
                 </Link>
 
                 <div>
@@ -71,17 +71,14 @@ const Navbar = () => {
                         user ? (
                             <img src={user} alt="" />
                         ) : (
-                            <button>
-                                <IoLogIn />
-                                <span>
-                                    Login
-                                </span>
-                            </button>
+                            <Link className='py-[8px] px-[30px] bg-[#6442f9] text-[#fff] rounded-md uppercase gap-[6px]' to={"/login"}>
+                                Login
+                            </Link>
                         )
                     }
                 </div>
 
-                <button onClick={handleToggle} className=''>
+                <button onClick={handleToggle} className='text-[28px] lg:hidden'>
                     {!open ? <IoMenu /> : <IoClose />}
                 </button>
             </div>
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className='container mx-auto lg:h-[90vh] hero-bg'>
+
         </div>
     );
 };
